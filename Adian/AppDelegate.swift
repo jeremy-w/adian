94a82ceb75e11fef72468cb8be1a18cd4f53b862
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-12-07 22:14:35 -0500

    Dig up ComposePostViewController and point it at wry
    Currently there's no feedback during or after posting,
    and you can only post once.
    
    But, by golly, [it posted!](https://alpha.app.net/jws/post/66485170)

--- Adian/AppDelegate.swift
+++ Adian/AppDelegate.swift
@@ -11,16 +11,30 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // What the heck? You can't connect an outlet to the main view controller,
+        // and you can't actually grab a list of view controllers anywhere.
+        //
+        // Guess it's fishing time!
+        //
+        // This is only temporary till we start vending compose post VCs properly, anyway.
+        guard let
+            window = NSApp.windows.first,
+            view = window.contentView,
+            viewController = view.nextResponder as? ComposePostViewController
+        else {
+            NSLog("failed to dig up compose post view controller")
+            return
+        }
+
+        configureComposePostViewController(viewController)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
     }
 
+    func configureComposePostViewController(vc: ComposePostViewController) {
+        vc.configure(Wry(task: FoundationTask()))
+    }
 
 }
-
